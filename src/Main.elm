82f73dce399195a5bf756d17137fa518e88d16f8
Lorenz Rosenthal <lorenz.rosenthal@cbc.de>
2019-12-20 18:53:53 +0100

    feat: add base layout and types

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,9 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Html exposing (Html, div, h1, img, text)
+import Html.Attributes as HtmlAttr exposing (src, style)
+
 
 
 ---- MODEL ----
@@ -38,8 +39,57 @@ view : Model -> Html Msg
 view model =
     div []
         [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+        , listView mockList
         ]
+
+
+type Minute
+    = Minute Int
+
+
+minute : Int -> Minute
+minute int =
+    Minute int
+
+
+type alias Todo =
+    { start : Minute
+    , duration : Minute
+    , title : String
+    }
+
+
+minuteToStr : Minute -> String
+minuteToStr (Minute m) =
+    String.fromInt m
+
+
+todoView : Todo -> Html Msg
+todoView { title, duration, start } =
+    Html.li
+        [ style "border" "1px solid black" ]
+        [ Html.input [ HtmlAttr.value title ] []
+        , Html.input [ HtmlAttr.value <| minuteToStr duration ] []
+        ]
+
+
+listView : List Todo -> Html Msg
+listView list =
+    Html.ul
+        []
+        (List.map todoView list)
+
+
+mockTodo =
+    { start = minute 0
+    , duration = minute 5
+    , title = "bla"
+    }
+
+
+mockList : List Todo
+mockList =
+    List.repeat 5 mockTodo
 
 
 
